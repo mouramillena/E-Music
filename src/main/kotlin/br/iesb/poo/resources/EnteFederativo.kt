62d0f676b53cab39192e954841b5219609ecf6f4
jessica <jessicarodrigues.cco@outlook.com>
2020-09-29 00:28:51 -0300

    Ajuste da classe Entefederativo e criacao da classe Regiao

--- src/main/kotlin/br/iesb/poo/resources/EnteFederativo.kt
+++ src/main/kotlin/br/iesb/poo/resources/EnteFederativo.kt
@@ -1,5 +1,5 @@
 package br.iesb.poo.resources
 
-open class EnteFederativo(var name: String) {
+open class EnteFederativo(var Cidade: String, val Sigla: String, val País: String, val Região: String) {
 }
 
---
+++ src/main/kotlin/br/iesb/poo/resources/Regiao.kt
@@ -0,0 +1,4 @@
+package br.iesb.poo.resources
+
+class Regiao(var name: String, var code_reg: Int): EnteFederativo(cidade){
+}
